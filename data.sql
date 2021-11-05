3038670b8f40203a6f061a674c448cd04e6bf4bf
JbirdL86 <jgonzalez.wj@gmail.com>
2021-11-04 18:13:32 -0600

    Add data to tables

--- data.sql
+++ data.sql
@@ -11,3 +11,57 @@ INSERT INTO animals values(default, 'Squirtle', '19930402', 3, false, -12.3);
 INSERT INTO animals values(default, 'Angemon', '20050612', 1, true, -45.0);
 INSERT INTO animals values(default, 'Boarmon', '20050607', 7, true, 20.4);
 INSERT INTO animals values(default, 'Blossom', '19981013', 3, true, 17.0);
+
+INSERT INTO owners values(default, 'Sam Smith', 34);
+INSERT INTO owners values(default, 'Jennifer Orwell', 19);
+INSERT INTO owners values(default, 'Bob', 45);
+INSERT INTO owners values(default, 'Melody Pond', 77);
+INSERT INTO owners values(default, 'Dean Winchester', 14);
+INSERT INTO owners values(default, 'Jodie Whittaker', 38);
+
+INSERT INTO species values('Pokemon');
+INSERT INTO species values ('Digimon');
+
+UPDATE animals
+SET species_id = species.id
+FROM species
+WHERE animals.name LIKE '%mon'
+  AND species.name LIKE 'Digimon';
+
+UPDATE animals
+SET species_id = species.id
+FROM species
+WHERE animals.species_id IS NULL
+  AND species.name LIKE 'Pokemon';
+
+UPDATE animals
+SET owner_id = owners.id
+FROM owners
+WHERE animals.name = 'Agumon'
+  AND owners.full_name = 'Sam Smith';
+
+UPDATE animals
+SET owner_id = owners.id
+FROM owners 
+WHERE animals."name" = 'Gabumon' 
+	AND owners.full_name = 'Jennifer Orwell'
+	OR animals."name" = 'Pikachu' 
+	AND owners.full_name = 'Jennifer Orwell';
+
+UPDATE animals
+SET owner_id = owners.id
+FROM owners 
+WHERE animals."name" = 'Devimon' 
+	AND owners.full_name = 'Bob'
+	OR animals."name" = 'Plantmon' 
+	AND owners.full_name = 'Bob';
+
+UPDATE animals
+SET owner_id = owners.id
+FROM owners 
+WHERE animals."name" = 'Charmander' 
+	AND owners.full_name = 'Melody Pond'
+	OR animals."name" = 'Squirtle' 
+	AND owners.full_name = 'Melody Pond'
+	OR animals."name" = 'Blossom' 
+	AND owners.full_name = 'Melody Pond';
