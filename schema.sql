b10f989fe446034be6afdf9926f933f89da71784
JbirdL86 <jgonzalez.wj@gmail.com>
2021-11-04 18:14:24 -0600

    Add columns to animals table and add species table and owners table

--- schema.sql
+++ schema.sql
@@ -10,3 +10,26 @@ CREATE TABLE animals(
 );
 
 alter table animals add species varchar(250);
+
+CREATE TABLE owners(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR(250),
+  age INT,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE species(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(250),
+  PRIMARY KEY(id)
+);
+
+alter table animals drop column species;
+
+alter table animals	
+	add column species_id INT
+references species(id);
+
+alter table animals	
+	add column owner_id INT
+references owners(id);
